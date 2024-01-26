ff04af1c9c709bb6b2de2e442b8f32acfa7a5ea5
unknown <pahu567@gmail.com>
2024-01-26 20:46:31 +0530

    final commit

--- Public/script.js
+++ Public/script.js
@@ -22,7 +22,8 @@ showChat.addEventListener("click", () => {
 const user = prompt("Enter your name");
 
 var peer = new Peer({
-  host: '127.0.0.1',
+  host: '172.20.10.10',
+  // host: '127.0.0.1',
   port: 3030,
   path: '/peerjs',
   config: {
